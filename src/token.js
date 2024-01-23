ed2efc68b54f26fae9e51880e4cf3a4fe3518fed
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-23 11:34:28 +0000

    done the questions

--- src/token.js
+++ src/token.js
@@ -1,34 +1,53 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+
+const rickPayload = {
+  id: 153,
+  username: "sanchez",
+  email: "rick@sanchez.com",
+  role: "ADMIN",
+};
+
+const rickSecret = "87764d1a-92dc-4ced-a758-9c898c31d525"
 
 /**
  * Implement this function to accept a payload and a secret key and return a JWT without an expiry time
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
  */
-function createToken() {
-    
+function createToken(payload, secretKey) {
+  return jwt.sign(payload, secretKey);
 }
 
 /**
  * Implement this function to accept a payload, secret key and an expiry time, and return a JWT with an expiry
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
  */
-function createTokenWithExpiry() {
-    
+function createTokenWithExpiry(payload, secretKey, expiryTime) {
+  return jwt.sign(payload, secretKey, { expiresIn: expiryTime });
 }
 
 /**
  * Implement this function to accept a JWT and a secret key. Return the decoded token (the payload) if verification is successful, and false if it fails
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+ *
+ * @param {String} webToken
+ * @param {import("jsonwebtoken").Secret} secretKey
  */
-function verifyToken() {
-
+function verifyToken(webToken, secretKey) {
+  try {
+    return jwt.verify(webToken, secretKey);
+  } catch (error) {
+    return false;
+  }
 }
 
 module.exports = {
-    createToken,
-    createTokenWithExpiry,
-    verifyToken
-}
+  createToken,
+  createTokenWithExpiry,
+  verifyToken,
+};
+
+const rick = createToken(rickPayload, rickSecret)
+console.log(rick)
